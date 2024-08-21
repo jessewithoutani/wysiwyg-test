d2ac1f6abc3696b8a7514945bb26229fa73e180b
Jesse <32250628+BlueberryTech123@users.noreply.github.com>
2024-08-21 01:38:15 -0700

    gguuuu

---
+++ index.js
@@ -0,0 +1,9 @@
+function bold() {
+    document.execCommand("bold", false, null);
+}
+function italic() {
+    document.execCommand("italic", false, null);
+}
+function underline() {
+    document.execCommand("underline", false, null);
+}
